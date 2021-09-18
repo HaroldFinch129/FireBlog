3cdcfb660f6e593d8553431a9d731535ba08edfa
Harold <m.h.gamze@gmail.com>
2021-09-18 14:13:13 +0300

    navbar router completed

--- fireblog-app/src/components/Navbar.js
+++ fireblog-app/src/components/Navbar.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="Navbar">
-            <Link to>
-            <h2>BLOG</h2>
-                </Link> 
+  return (
+    <div className="Navbar">
+      <Link to='/'>
+        <h2>BLOG</h2>
+      </Link>
 
-            <Link to a href= "/Login">Login</Link>
-            <Link to a href= "/Register">Register</Link>
+      <Link to="/Login">
+        Login
+      </Link>
+      <Link to="/Register">
+        Register
+      </Link>
+    </div>
+  );
+};
 
-
-
-
-            
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
