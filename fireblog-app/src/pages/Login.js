9ab1eabf01af392821987abd009fa9a52b9e5c6c
Harold <m.h.gamze@gmail.com>
2021-09-21 13:32:50 +0300

    login-css-3

--- fireblog-app/src/pages/Login.js
+++ fireblog-app/src/pages/Login.js
@@ -8,10 +8,11 @@ const Login = () => {
             <div className="form_container">
                 <form action="" className="Login_form">
                     <div className="form_image"></div>
-                    <input type="text" />
-                    <input type="text" />
-                    <button>login</button>
-                    <button>with google</button>
+                    <h1 className="form_header">LOGIN</h1>
+                    <input type="text" className="form_input"/>
+                    <input type="text" className="form_input"/>
+                    <button className="form_button">login</button>
+                    <button className="form_button">with google</button>
 
                 </form>
 
