d1be63bb740dc675d4aca71cf4b4ca11de9a1fe1
Harold <m.h.gamze@gmail.com>
2021-09-23 23:52:03 +0300

    newblog.css changing

--- fireblog-app/src/pages/NewBlog.js
+++ fireblog-app/src/pages/NewBlog.js
@@ -4,14 +4,14 @@ import './NewBlog.css';
 const NewBlog = () => {
     return (
         <div className="NewBlog_container">
-      <div className="form_container">
-        <form action="" className="Login_form">
-          <div className="form_image"></div>
-          <h1 className="form_header">NEW BLOG</h1>
+      <div className="NewLog_form_container">
+        <form action="" className="NewLog_form">
+          <div className="NewLog_image"></div>
+          <h1 className="NewBlog_form_header">NEW BLOG</h1>
           <input type="text" className="form_input" placeholder="Email" />
           <input type="password" className="form_input" placeholder="Password" />
           <div className="content_container" >
-              <textarea placeholder="Content" id="content" cols="32" rows="7"></textarea>
+              <textarea placeholder="Content" id="content" cols="30" rows="4"></textarea>
           </div>
           <button className="form_button" value = "LOGIN" >
             SUBMIT{" "}
